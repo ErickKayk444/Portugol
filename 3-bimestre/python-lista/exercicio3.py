b7a4b8f217f991d5c063be1bac9285f577937e05
ErickKayk444 <erick.kayk.ff@gmail.com>
2026-08-28 12:18:30 -0300

    Delete 3-bimestre/python-lista/exercicio3.py

--- 3-bimestre/python-lista/exercicio3.py
+++
@@ -1,8 +0,0 @@
-contador = 1
-numero = int(input("Digite um número inteiro positivo: "))
-if numero > 0:
-    while contador <= numero:
-        print(contador)
-        contador += 1
-else:
-    print("Por favor, digite um número inteiro positivo.")
